f2e3d4816595f2080c0460cfef96f32814b8015a
Tech-Applywizz-git-account <techteam@Applywizz.com>
2025-11-06 11:43:49 +0530

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -876,8 +876,7 @@ const HomePage: React.FC = () => {
             <div className="grid md:grid-cols-3 gap-8 justify-center">
               {[
                 {
-                  title: "Sign up for an account
- ",
+                  title: "Sign up for an account",
                   desc: "Sign up today and explore all verified H1B sponsors - forever.",
                   bgColor: "bg-blue-50",
                   image: "/add-user.svg",

fb862f6c26424fc8efa9e9137c1794e1bf657a6a
Tech-Applywizz-git-account <techteam@Applywizz.com>
2025-11-25 20:35:08 +0530

    sanjay Payment

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -66,7 +66,7 @@ const SignupPage: React.FC = () => {
             email: form.email,
             phone: form.phone,
             country_code: form.countryCode,
-            promo_code: "sushmitha",
+            promo_code: "Sanjay",
           }
         ]);
 
